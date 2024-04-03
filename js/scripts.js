30c63cddd4fb06a38621ec7575db69250663438e
Michael D Havis <50241647+havis-michael@users.noreply.github.com>
2024-04-03 10:36:07 -0400

    Add function to play a single round
    Add playRound function. Allow the player's input to be case
    insensitive. Throw an error if the player chooses an option other than
    rock, paper or scissors.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,40 @@ function getComputerChoice() {
 
   return computerChoice;
 }
+
+function playRound(playerChoice, computerChoice) {
+  let player = playerChoice.toLowerCase();
+  let computer = computerChoice.toLowerCase();
+  let playerWinner = 0;
+  let winnerMessage = "";
+
+  if (player === computer) {
+    playerWinner = 2;
+  } else if (player === "rock") {
+    if (computer === "scissors") {
+      playerWinner = 1;
+    }
+  } else if (player === "paper") {
+    if (computer === "rock") {
+      playerWinner = 1;
+    }
+  } else if (player === "scissors") {
+    if (computer === "paper") {
+      playerWinner = 1;
+    }
+  } else {
+    playerWinner = -1;
+  }
+
+  if (playerWinner === 1) {
+    winnerMessage = `You Win! ${playerChoice} beats ${computerChoice}!`;
+  } else if (playerWinner === 0) {
+    winnerMessage = `You Lose! ${computerChoice} beats ${playerChoice}!`;
+  } else if (playerWinner === 2) {
+    winnerMessage = `You Tied! Computer chose ${computerChoice} too!`;
+  } else {
+    winnerMessage = `${playerChoice} is not a valid option. Try again by choosing 'Rock', 'Paper', or 'Scissors'.`;
+  }
+
+  return winnerMessage;
+}
